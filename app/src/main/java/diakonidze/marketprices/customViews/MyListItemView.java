9bf5b6a12e1425715a093c9bc912c6753c2eab47
kartlos diakonidze <kartlos99@yahoo.com>
2019-03-26 20:09:09 +0400

    MyListItem ჩასწორება ახალ სტრუქტურაზე

--- app/src/main/java/diakonidze/marketprices/customViews/MyListItemView.java
+++ app/src/main/java/diakonidze/marketprices/customViews/MyListItemView.java
@@ -2,13 +2,11 @@ package diakonidze.marketprices.customViews;
 
 import android.content.Context;
 import android.graphics.Paint;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -22,7 +20,7 @@ public class MyListItemView extends ConstraintLayout {
 
     private Context context;
     private LinearLayout uncheckedConteiner, checkedConteiner;
-    private RealProduct product;
+    private RealProduct realProduct;
     private Boolean isChecked;
 
     private static final String TAG = "My_List_ItemView";
@@ -32,12 +30,12 @@ public class MyListItemView extends ConstraintLayout {
     private Button btn_itemRemove;
     private TextView tv_dasaxeleba, tv_magazia, tv_price;
 
-    public MyListItemView(final Context context, final RealProduct product, Boolean isChecked, final LinearLayout uncheckedConteiner, final LinearLayout checkedConteiner) {
+    public MyListItemView(final Context context, final RealProduct realproduct, Boolean isChecked, final LinearLayout uncheckedConteiner, final LinearLayout checkedConteiner) {
         super(context);
         this.context = context;
         this.uncheckedConteiner = uncheckedConteiner;
         this.checkedConteiner = checkedConteiner;
-        this.product = product;
+        this.realProduct = realproduct;
 //        this.isChecked = isChecked;
         Log.d(TAG, "MyListItemView Constructor");
         initView();
@@ -46,19 +44,19 @@ public class MyListItemView extends ConstraintLayout {
             @Override
             public void onClick(View v) {
                 boolean isChecked = checkBox.isChecked();
-                product.setChecked(isChecked);
+                realproduct.setChecked(isChecked);
 
                 if (isChecked) {
                     Log.d(TAG, "oncheck_click : 91");
                     tv_dasaxeleba.setPaintFlags(tv_dasaxeleba.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
                     uncheckedConteiner.removeView(getView());
-                    MyListItemView copy = new MyListItemView(context, product, true, uncheckedConteiner, checkedConteiner);
+                    MyListItemView copy = new MyListItemView(context, realproduct, true, uncheckedConteiner, checkedConteiner);
                     checkedConteiner.addView(copy);
                 } else {
                     Log.d(TAG, "oncheck_click: 92");
                     tv_dasaxeleba.setPaintFlags(tv_dasaxeleba.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
                     checkedConteiner.removeView(getView());
-                    MyListItemView copy = new MyListItemView(context, product, false, uncheckedConteiner, checkedConteiner);
+                    MyListItemView copy = new MyListItemView(context, realproduct, false, uncheckedConteiner, checkedConteiner);
                     uncheckedConteiner.addView(copy);
                 }
             }
@@ -74,7 +72,7 @@ public class MyListItemView extends ConstraintLayout {
                     uncheckedConteiner.removeView(getView());
                 }
                 for (int i = 0; i < GlobalConstants.MY_SHOPING_LIST.size(); i++) {
-                    if (GlobalConstants.MY_SHOPING_LIST.get(i).getId() == product.getId()) {
+                    if (GlobalConstants.MY_SHOPING_LIST.get(i).getId() == realproduct.getId()) {
                         GlobalConstants.MY_SHOPING_LIST.remove(i);
                     }
                 }
@@ -92,12 +90,12 @@ public class MyListItemView extends ConstraintLayout {
         tv_magazia = parentView.findViewById(R.id.tv_magazia);
         tv_price = parentView.findViewById(R.id.tv_prod_price);
 
-        tv_dasaxeleba.setText(product.getProduct_name());
-        tv_magazia.setText(product.getMarketName());
-        tv_price.setText(String.valueOf(product.getPrice()));
+        tv_dasaxeleba.setText(realProduct.getProduct().getName());
+        tv_magazia.setText(realProduct.getMarketName());
+        tv_price.setText(String.valueOf(realProduct.getPrice()));
 
-        checkBox.setChecked(product.getChecked());
-        setState(product.getChecked());
+        checkBox.setChecked(realProduct.getChecked());
+        setState(realProduct.getChecked());
     }
 
     public View getView() {
